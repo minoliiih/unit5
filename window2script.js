012beedca3506ea3963d1455490ff49d717f3012
Mina <elifminayilmazz@gmail.com>
2025-10-16 23:52:09 +0100

    Add files via upload

---
+++ window2script.js
@@ -0,0 +1,5 @@
+document.getElementById('not-robot').addEventListener('change', function() {
+    if (this.checked) {
+        window.location.href = "window2page.html"; // your target HTML file
+    }
+});
